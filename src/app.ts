0ef3c5fca316224f9f556efec5c17fc6cdba733f
isalim079 <salimreza1025@gmail.com>
2025-07-18 15:58:16 +0600

    Cors origin updated

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import playlistRoutes from './routes/playlist.route';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import playlistRoutes from "./routes/playlist.route";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://yt-downloader-frontend-sand.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use('/api/playlist', playlistRoutes);
+app.use("/api/playlist", playlistRoutes);
 
-app.get('/', (_req, res) => {
-  res.send('YouTube Playlist Downloader API is running');
+app.get("/", (_req, res) => {
+  res.send("YouTube Playlist Downloader API is running");
 });
 
 const PORT = process.env.PORT || 5000;
